115d0c41b6920a3d8c1f34633fb4fb04a42f9e73
Celine Pineau <pineau.celine@gmail.com>
2024-04-08 13:42:00 +0200

    creation modele hotel

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "about", to: "pages#about"
 
   resources :hotels, only: %i[new create index show destroy]
+  resources :users, only: [:show]
+
+  get "profile", to: "users#profile"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 # Clear existing data
 Hotel.destroy_all
-# Seed recettes
-hotel_1 = Hotel.create(title: "Villa de rêve 1", descrition: "Vue sur la mer", price: 100)
-hotel_2 = Hotel.create(title: "Villa de rêve 2", descrition: "Vue sur la mer", price: 200)
-hotel_3 = Hotel.create(title: "Villa de rêve 3", descrition: "Vue sur la mer", price: 300)
+User.destroy_all
+
+# Seed Hotels
+hotel_1 = Hotel.create(title: "Villa de rêve 1", description: "Vue sur la mer", price: 100)
+hotel_2 = Hotel.create(title: "Villa de rêve 2", description: "Vue sur la montagne", price: 200)
+hotel_3 = Hotel.create(title: "Villa de rêve 3", description: "Vue sur la ville", price: 300)
+
+# Créer des seeds de Users
+user_1 = User.create(firstname: 'John', lastname: 'Doe', email: 'john@test.com', password: '123456')
+user_2 = User.create(firstname: 'Jane', lastname: 'Doe', email: 'jane@test.com', password: '123456')
+user_3 = User.create(firstname: 'Me', lastname: 'Nala', email: 'martine@test.com', password: '123456')
