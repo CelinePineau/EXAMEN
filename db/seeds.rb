a3e39486f340eb1e77f1c650ba7ed55bf1a59e2a
Celine Pineau <pineau.celine@gmail.com>
2024-04-08 19:12:39 +0200

    creation modele hotel

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@ User.destroy_all
 hotel_1 = Hotel.create(title: "Villa de rêve 1", description: "Vue sur la mer", price: 100)
 hotel_2 = Hotel.create(title: "Villa de rêve 2", description: "Vue sur la montagne", price: 200)
 hotel_3 = Hotel.create(title: "Villa de rêve 3", description: "Vue sur la ville", price: 300)
+hotel_4 = Hotel.create(title: "Villa de rêve 4", description: "Vue sur le lac", price: 400)
+hotel_5 = Hotel.create(title: "Villa de rêve 5", description: "Vue sur le train", price: 500)
+hotel_6 = Hotel.create(title: "Villa de rêve 6", description: "Vue sur la campagne", price: 600)
 
 # Créer des seeds de Users
 user_1 = User.create(firstname: 'John', lastname: 'Doe', email: 'john@test.com', password: '123456')
